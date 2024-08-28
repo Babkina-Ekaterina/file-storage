827d5dd50dd099a966412938354afbe0b681af70
Babkina-Ekaterina <bkatya5577@yandex.ru>
2024-08-28 09:50:44 +0300

    Add file getting feature

--- src/main/java/caselab/test/file_storage/service/FileService.java
+++ src/main/java/caselab/test/file_storage/service/FileService.java
@@ -1,10 +1,14 @@
 package caselab.test.file_storage.service;
 
 import caselab.test.file_storage.data.dto.FileCreateDto;
+import caselab.test.file_storage.data.dto.FileGetDto;
+import caselab.test.file_storage.data.entity.FileEntity;
 import caselab.test.file_storage.data.mapper.FileMapper;
 import caselab.test.file_storage.data.repository.FileRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
@@ -14,5 +18,14 @@ public class FileService {
 
     public Long createFile(FileCreateDto fileCreateDto) {
         return fileRepository.save(fileMapper.fileCreateDtoToFileEntity(fileCreateDto)).getId();
+    }
+
+    public Optional<FileGetDto> getFile(Long id) {
+        Optional<FileEntity> fileEntityOptional = fileRepository.findById(id);
+
+        if (fileEntityOptional.isPresent()) {
+            return fileEntityOptional.map(entity -> fileMapper.fileEntityToFileGetDto(entity));
+        }
+        return Optional.empty();
     }
 }
